c8f3d28014e4cc43f5f95432a01c356b8b2cc795
Crescenteea <soarmei@gmail.com>
2020-06-09 09:12:39 +0900

    초기표시 설정

---
+++ public/js/app.js
@@ -0,0 +1,10 @@
+// 상태
+let todos = [];
+
+window.onload = () => {
+  todos = [
+    { id: 1, content: 'HTML', completed: false },
+    { id: 2, content: 'CSS', completed: true },
+    { id: 3, content: 'Javascript', completed: false }
+  ];
+};
