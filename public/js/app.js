592c13aed3996d70408e173bd2b41c50ed8b4819
Crescenteea <soarmei@gmail.com>
2020-06-09 14:39:33 +0900

    렌더 함수 수정

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,7 @@
 // 상태
 let todos = [];
+let _todos = [];
+let nav = 'all';
 
 // DOM Nodes
 const $todos = document.querySelector('.todos');
@@ -8,34 +10,84 @@ const $nav = document.querySelector('.nav');
 // 함수 영역
 const render = () => {
   let html = '';
-  todos.forEach(({ id, content, completed }) => {
+
+  // if (nav === 'active') {
+  //  _todos = todos.filter(todo => !todo.completed);
+  // };
+
+  // if (nav === 'completed') {
+  //   _todos = todos.filter(todo => todo.completed);
+  //  };
+
+
+  // _todos = todos.filter(todo => {
+  //   nav === 'active' || nav === 'completed' ? (!todo.completed ? !todo.completed : todo.completed ) : todo.completed
+  // )});
+
+  _todos = todos.filter(todo => nav === 'active' || nav === 'completed' ? !todo.completed : (todo.completed ? todo.completed : !todo.completed));
+
+
+  if (nav === 'all') {
+    _todos = todos;
+  };
+
+
+  _todos.forEach(({ id, content, completed }) => {
     html += `<li id="${id}" class="todo-item">
-        <input id="ck-${id}" class="checkbox" ${completed ? 'checked' : ''} type="checkbox">
-        <label for="ck-${id}">${content}</label>
-        <i class="remove-todo far fa-times-circle"></i>
-        </li>`;
+    <input id="ck-${id}" class="checkbox" ${completed ? 'checked' : ''} type="checkbox">
+    <label for="ck-${id}">${content}</label>
+    <i class="remove-todo far fa-times-circle"></i>
+    </li>`;
   });
-  console.log(todos);
   $todos.innerHTML = html;
 };
+
+const getTodos = () => {
+  todos = [
+    { id: 1, content: 'HTML', completed: false },
+    { id: 2, content: 'CSS', completed: true },
+    { id: 3, content: 'Javascript', completed: false }
+  ];
+
+  render();
+};
+
+// const activeTodos = () => {
+//   todos.filter(todo => todo.completed === false);
+//   render();
+// };
+
+
+// const completedTodos = () => {
+//   todos.filter(todo => todo.completed === true);
+//   render();
+// };
 
 const navTabList = target => {
   const navItem = [...$nav.children].filter($navItem => $navItem.matches('.active'));
   navItem[0].classList.remove('active');
 
   target.classList.add('active');
-};
 
-// 이벤트 핸들러 영역
-window.onload = () => {
-  todos = [
-    { id: 1, content: 'HTML', completed: false },
-    { id: 2, content: 'CSS', completed: true },
-    { id: 3, content: 'Javascript', completed: false }
-  ];
-  console.log(todos);
+// --------------------------------------------------------------------
+  nav = target.id;
+
+// nav tab 중 하나가 클릭되면
+// active가 target일 경우
+// target의 id가 active이면 filter해서 completed 값이 false인 것들만 그린다
+  // if (target.id === 'active') {
+  //   activeTodos();
+  // };
+
+  // if (target.id === 'completed') {
+  //   completedTodos();
+  // };
   render();
 };
+
+
+// 이벤트 핸들러 영역
+window.onload = getTodos;
 
 $nav.onclick = ({ target }) => {
   if (!target.matches('.nav > li:not(.active)')) return;
